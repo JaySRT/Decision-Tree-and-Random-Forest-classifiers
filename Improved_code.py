9faec460674256b2d01d9676514fda54a89f23b6
JaySRT <48157133+JaySRT@users.noreply.github.com>
2020-12-30 00:06:52 -0600

    Update Improved_code.py
    Not finished on working for this part.

--- Improved_code.py
+++ Improved_code.py
@@ -1,1 +1,147 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_name = sys.argv[1]
+df = pd.read_csv(file_name)
+print(df.head())
+
+df.info()
+
+df.drop(['EmployeeCount', 'EmployeeNumber', 'Over18', 'StandardHours'], axis="columns", inplace=True)
+
+categorical_col = []
+for column in df.columns:
+    if df[column].dtype == object and len(df[column].unique()) <= 50:
+        categorical_col.append(column)
+        print(f"{column} : {df[column].unique()}")
+        print("====================================")
+
+df['Attrition'] = df.Attrition.astype("category").cat.codes
+df.Attrition.value_counts()
+
+# Visulazing the distibution of the data for every feature
+df.hist(edgecolor='black', linewidth=1.2, figsize=(20, 20));
+
+#Correlation Matrix
+plt.figure(figsize=(30, 30))
+sns.heatmap(df.corr(), annot=True, cmap="RdYlGn", annot_kws={"size":15})
+
+#Data Processing (Split train, test data and labels)
+
+categorical_col.remove('Attrition')
+
+# Transform categorical data into dummies
+# categorical_col.remove("Attrition")
+# data = pd.get_dummies(df, columns=categorical_col)
+# data.info()
+from sklearn.preprocessing import LabelEncoder
+
+label = LabelEncoder()
+for column in categorical_col:
+    df[column] = label.fit_transform(df[column])
+
+from sklearn.model_selection import train_test_split
+
+X = df.drop('Attrition', axis=1)
+y = df.Attrition
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+#Applying the ML Algorithms
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+
+def print_score(clf, X_train, y_train, X_test, y_test, train=True):
+    if train:
+        pred = clf.predict(X_train)
+        print("Train Result:\n===========================================")
+        print(f"accuracy score: {accuracy_score(y_train, pred):.4f}\n")
+        print(f"Classification Report: \n \tPrecision: {precision_score(y_train, pred)}\n\tRecall Score: {recall_score(y_train, pred)}\n\tF1 score: {f1_score(y_train, pred)}\n")
+        print(f"Confusion Matrix: \n {confusion_matrix(y_train, clf.predict(X_train))}\n")
+        
+    elif train==False:
+        pred = clf.predict(X_test)
+        print("Test Result:\n===========================================")        
+        print(f"accuracy score: {accuracy_score(y_test, pred)}\n")
+        print(f"Classification Report: \n \tPrecision: {precision_score(y_test, pred)}\n\tRecall Score: {recall_score(y_test, pred)}\n\tF1 score: {f1_score(y_test, pred)}\n")
+        print(f"Confusion Matrix: \n {confusion_matrix(y_test, pred)}\n")
+
+
+########## Decision Tree Classifier ############
+from sklearn.tree import DecisionTreeClassifier
+
+tree_clf = DecisionTreeClassifier(random_state=42)
+tree_clf.fit(X_train, y_train)
+
+print_score(tree_clf, X_train, y_train, X_test, y_test, train=True)
+print_score(tree_clf, X_train, y_train, X_test, y_test, train=False)
+
+
+#### Decision Tree Classifier Hyperparameter tuning - Evaluation of Model Accuracy ####
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+
+params = {
+    "criterion":("gini", "entropy"), 
+    "splitter":("best", "random"), 
+    "max_depth":(list(range(1, 20))), 
+    "min_samples_split":[2, 3, 4], 
+    "min_samples_leaf":list(range(1, 20)), 
+}
+
+tree_clf = DecisionTreeClassifier(random_state=42)
+tree_cv = GridSearchCV(tree_clf, params, scoring="accuracy", n_jobs=-1, verbose=1, cv=3)
+tree_cv.fit(X_train, y_train)
+best_params = tree_cv.best_params_
+print(f"Best paramters: {best_params})")
+
+tree_clf = DecisionTreeClassifier(**best_params)
+tree_clf.fit(X_train, y_train)
+print_score(tree_clf, X_train, y_train, X_test, y_test, train=True)
+print_score(tree_clf, X_train, y_train, X_test, y_test, train=False)
+
+
+########## Random Forest ############
+from sklearn.ensemble import RandomForestClassifier
+
+rf_clf = RandomForestClassifier(n_estimators=100)
+rf_clf.fit(X_train, y_train)
+
+print_score(rf_clf, X_train, y_train, X_test, y_test, train=True)
+print_score(rf_clf, X_train, y_train, X_test, y_test, train=False)
+
+
+#### Random Forest Classifier Hyperparameter tuning - Evaluation of Model Accuracy ####
+
+## Grid Search Cross Validation ##
+
+n_estimators = [100, 500, 1000, 1500]
+max_features = ['auto', 'sqrt']
+max_depth = [2, 3, 5]
+max_depth.append(None)
+min_samples_split = [2, 5, 10]
+min_samples_leaf = [1, 2, 4, 10]
+bootstrap = [True, False]
+
+params_grid = {'n_estimators': n_estimators, 'max_features': max_features,
+               'max_depth': max_depth, 'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf, 'bootstrap': bootstrap}
+
+rf_clf = RandomForestClassifier(random_state=42)
+
+rf_cv = GridSearchCV(rf_clf, params_grid, scoring="accuracy", cv=3, verbose=2, n_jobs=-1)
+
+
+rf_cv.fit(X_train, y_train)
+best_params = rf_cv.best_params_
+print(f"Best parameters: {best_params}")
+
+rf_clf = RandomForestClassifier(**best_params)
+rf_clf.fit(X_train, y_train)
+
+print_score(rf_clf, X_train, y_train, X_test, y_test, train=True)
+print_score(rf_clf, X_train, y_train, X_test, y_test, train=False)
 
